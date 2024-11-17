54f8d9de0641faa0c7e9b745854e8a86ecc8578c
Yurka2012 <tsapiy.yurka@gmail.com>
2024-11-17 17:29:33 +0200

    likes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 import Handlebars from 'handlebars';
 import axios from 'axios';
 
+const postsContainer = document.querySelector('.posts');
+const postsSection = document.querySelector('.posts');
+
 async function loadPosts() {
-    try {
-        const response = await axios.get('http://localhost:1488/posts');
-        const data = response.data;
-        const postsContainer = document.querySelector('.posts');
+  try {
+    const response = await axios.get('http://localhost:1488/posts');
+    const data = response.data;
+    const template = document.querySelector('.template').innerHTML;
+    const compiledTemplate = Handlebars.compile(template);
 
-        postsContainer.innerHTML = '';
+    postsContainer.innerHTML =
+      data.length > 0
+        ? data.map(post => compiledTemplate({ post })).join('')
+        : 'No posts available';
+  } catch (error) {
+    console.error('Error loading posts:', error);
+    postsContainer.innerHTML = 'Error loading posts. Please try again later.';
+  }
+}
 
-        const template = document.querySelector('.template').innerHTML;
-        const compiledTemplate = Handlebars.compile(template);
+function addLikes(e) {
+  if (!e.target.classList.contains('like')) {
+    return;
+  }
 
-        postsContainer.innerHTML = data.length > 0 ?
-            data.map(post => compiledTemplate({ post })).join('') :
-            'No posts available';
-    } catch (error) {
-        console.error('Error loading posts:', error);
-        const postsContainer = document.querySelector('.posts');
-        postsContainer.innerHTML = 'Error loading posts. Please try again later.';
-    }
+  const postId = e.target.getAttribute('data-id');
+
+  axios.get(`http://localhost:1488/posts/${postId}`).then(response => {
+    let likes = response.data.likes;
+
+    axios.patch(`http://localhost:1488/posts/${postId}`, {
+      likes: (likes += 1),
+    });
+    loadPosts();
+  });
 }
 
 loadPosts();
+postsSection.addEventListener('click', addLikes);
